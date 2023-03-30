5d10421dce05b919ed00abcc04d87ba423b0ef64
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2023-03-30 23:52:51 +0900

    コンポーネント化して、propsで値を渡せるように変更

--- news-app/App.js
+++ news-app/App.js
@@ -1,25 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, Image, View } from "react-native";
+import { ListItem } from "./components/ListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: "https://picsum.photos/id/10/300/300",
-            }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            ReactNews ReactNews ReactNews ReactNews
-          </Text>
-          <Text style={styles.subText}>仮文言</Text>
-        </View>
-      </View>
+      <ListItem 
+        imageURL={"https://picsum.photos/id/10/300/300"}
+        title="ホゲホゲ"
+        author="React News"
+      />
+      <ListItem 
+        imageURL={"https://picsum.photos/id/20/300/300"}
+        title="フガフガ"
+        author="React News"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -28,29 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#eee",
     alignItems: "center",
     justifyContent: "center",
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    backgroundColor: "white",
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between"
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
   },
 });
