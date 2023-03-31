413a67708f599eedb279846a44ad0fedabe439f5
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2023-04-01 00:01:17 +0900

    safeAreaViewを導入し、ノッチにコンポーネントがかからないようにした。

--- news-app/App.js
+++ news-app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Image, View, ScrollView } from "react-native";
+import { StyleSheet, Text, Image, SafeAreaView, ScrollView } from "react-native";
 import { ListItem } from "./components/ListItem";
 import articles from "./dummies/articles";
 
@@ -16,10 +16,10 @@ export default function App() {
   });
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ScrollView>{items}</ScrollView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
