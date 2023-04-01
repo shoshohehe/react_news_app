5a4e8d859875749aa058ed5922c1e319e1a154ba
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2023-04-01 23:15:51 +0900

    ScrollViewをFlatListに変更
    ・不要なimportの削除
    ・FlatListは遅延レンダリングを実施する（参考：https://reactnative.dev/docs/scrollview）

--- news-app/App.js
+++ news-app/App.js
@@ -1,23 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Image, SafeAreaView, ScrollView } from "react-native";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { ListItem } from "./components/ListItem";
 import articles from "./dummies/articles";
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return (
-      <ListItem
-        imageURL={article.urlToImage}
-        title={article.title}
-        author={article.author}
-        key={index.toString()}
-      />
-    );
-  });
-
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>{items}</ScrollView>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem imageURL={item.urlToImage} title={item.title} author={item.author} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
