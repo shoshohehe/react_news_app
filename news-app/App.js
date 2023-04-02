e80a2b41126796cf920e20eec80014ca0b23f212
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2023-04-02 16:50:44 +0900

    APIからニュース情報を取得するように変更

--- news-app/App.js
+++ news-app/App.js
@@ -1,15 +1,34 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { ListItem } from "./components/ListItem";
-import articles from "./dummies/articles";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+  //画面初期化時に一回だけ実行
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={articles}
         renderItem={({ item }) => (
-          <ListItem imageURL={item.urlToImage} title={item.author} author={item.author} />
+          <ListItem imageURL={item.urlToImage} title={item.title} author={item.author} />
         )}
         keyExtractor={(item, index) => index.toString()}
       />
