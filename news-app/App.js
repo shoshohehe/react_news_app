2754393dd3cc70296b1008fb0528feaee121c0a8
shoshohehe <65958978+shoshohehe@users.noreply.github.com>
2023-05-06 17:35:07 +0900

    BottomTabNavigatorの導入
    FontAwesomeを使用し、デフォルトのアイコンを変更

--- news-app/App.js
+++ news-app/App.js
@@ -1,17 +1,38 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
 import { HomeScreen } from "./screens/HomeScreen";
 import { ArticleScreen } from "./screens/ArticleScreen";
+import { ClipScreen } from "./screens/ClipScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    if (route.name === "HomeTab") {
+      return <FontAwesome name="home" size={size} color={color} />;
+    } else if (route.name === "ClipTab") {
+      return <FontAwesome name="bookmark" size={size} color={color} />;
+    }
+  },
+});
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Article" component={ArticleScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
+        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
